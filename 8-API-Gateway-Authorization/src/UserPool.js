0b65a718265c249ea50c01008e76cba223f49985
inman <sf990668852@gmail.com>
2021-05-30 22:35:56 +0800

    update

--- 8-API-Gateway-Authorization/src/UserPool.js
+++ 8-API-Gateway-Authorization/src/UserPool.js
@@ -1,8 +1,14 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
+  UserPoolId: 'us-east-2_OkftE0LZQ',
+  ClientId: '3gvjfu283dugo2iq4b6p7ac65q'
+};
+
+/*
+const poolData = {
   UserPoolId: 'us-east-1_8WMsl8AMg',
   ClientId: '41qemsprq6sqqjraln2kqr04k7'
 };
-
+*/
 export default new CognitoUserPool(poolData);
